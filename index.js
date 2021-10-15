48b053881c2320a960fe1bbe3fe2736a2b22b069
Anthony_Dellavecchia <leeznon@gmail.com>
2021-10-15 14:10:00 -0500

    feat(customize): customize theme

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ handlebars.registerHelper({
 
   formatDate: function (date) {
     return moment(date).format('MM/YYYY');
+  },
+
+  formatDateEdu: function (date) {
+    return moment(date).format('YYYY');
+  },
+
+  ifEquals: function (arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+  },
+
+  ifNotEquals: function (arg1, arg2, options) {
+    return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
   }
 });
 
